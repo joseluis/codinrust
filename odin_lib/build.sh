93ac1ca17f7a622fef14848d687e8a53d13cf235
joseLuís <joseluis@andamira.net>
2023-08-03 14:08:19 +0200

    fix odin static lib

--- odin_lib/build.sh
+++ odin_lib/build.sh
@@ -8,7 +8,7 @@ odin build odin_lib.odin -file -build-mode:dll -out:libodin_lib_shared
 
 # compile static lib
 echo "  compiling static lib (*.a | *.lib) . . ."
-odin build odin_lib.odin -file -build-mode:obj -out:libodin_lib_static
+odin build odin_lib.odin -file -build-mode:obj -reloc-mode:pic -out:libodin_lib_static
 ar rcs libodin_lib_static.a libodin_lib_static.o
 rm libodin_lib_static.o
 
